aa38972b8b5249acdccd8c73232ca9c66c092eae
David Mason <DIMason1702@gmail.com>
2023-09-30 16:04:01 +0100

    Header complete (minus styling), Daily sprint plan made

--- main.js
+++ main.js
@@ -1,8 +1,14 @@
-// Variable location
+// // Variable location
+// Header label variable
 const locationLabel = document.getElementById("location-label");
+// Header location input variable
 const inputBox = document.getElementById("location-input");
+// Header submit button variable
 const submit = document.getElementById("submit");
+// Header redo image tag variable
 const redo = document.getElementById("redo");
+// Header weather image type variable
+const weatherImage = document.getElementById("weather-image");
 
 // Location generating function
 async function localGen (userInput) {
@@ -34,24 +40,36 @@ function displayWeather (weatherData) {
     const headerTemp = document.getElementById("headerTemp");
     // Change the text content of the headerTemp id to the current temp
     headerTemp.textContent = `${weatherData.current_weather.temperature}`;
+    // Call weather image type setter
+    weatherImageSetter(weatherData.current_weather.weathercode)
+    // Case correction on inputted value to a variable
+    const locationName = caseFixer(inputBox.value)
+    // Add user location to header
+    locationLabel.textContent += locationName;
+    // Remove input box
+    inputBox.style.display = "none";
+}
 
-    // Create a const for the weather-image img tag
-    const weatherImage = document.getElementById("weather-image");
+function weatherImageSetter (weatherCode) {
     // Sets the img src to a png
     // Switch function to set image depending on the weather code
-    switch (weatherData.current_weather.weathercode) {
+    switch (weatherCode) {
+        // Sunny
         case 0:
             weatherImage.src = "sun.png";
             break;
+        // Cloudy
         case 1:
         case 2:
         case 3:
             weatherImage.src = "partly-cloudy-day.png";
             break;
+        // Foggy or Hazay
         case 45:
         case 48:
             weatherImage.src = "haze.png";
             break;
+        // Raining
         case 51:
         case 53:
         case 55:
@@ -67,6 +85,7 @@ function displayWeather (weatherData) {
         case 82:
             weatherImage.src = "rain.png";
             break;
+        // Snowing
         case 71:
         case 73:
         case 75:
@@ -74,21 +93,29 @@ function displayWeather (weatherData) {
         case 86:
             weatherImage.src = "snow.png";
             break;
+        // Hail
         case 77:
             weatherImage.src = "hail.png";
             break;
+        // Storm
         case 95:
         case 96:
         case 99:
             weatherImage.src = "storm.png";
             break;
     }
-
-    // Add user location to header
-    locationLabel.textContent += inputBox.value;
-    // Remove input box
-    inputBox.style.display = "none";
 }
+
+// Function to put proper case on user input
+function caseFixer (location) {
+    // Convert string to an array all in lower case
+    const letterArray = location.toLowerCase().split("");
+    // Set the first letter in the array to uppercase
+    letterArray[0] = letterArray[0].toUpperCase();
+    // Join the array into a string with no commas and return
+    return letterArray.join("");
+}
+
 
 // Call and display function
 async function callAndDisplay (userInput) {
